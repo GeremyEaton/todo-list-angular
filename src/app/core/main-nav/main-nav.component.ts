bcf97f1df093af93e10d1838ca8b34e7620cdad4
Geremy <geremy.eaton@web-atrio.com>
2019-01-21 09:18:38 +0100

    feat(core/main-nav): improve main-nav component

--- src/app/core/main-nav/main-nav.component.ts
+++ src/app/core/main-nav/main-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-nav',
