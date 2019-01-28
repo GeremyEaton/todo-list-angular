4253c4ca876a7de5ee2b189c403f143a69f1435b
Geremy <geremy.eaton@web-atrio.com>
2019-01-28 11:17:21 +0100

    feat(deployement): Add configuration for deploy and configuration's server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBHGuNXzwHcYKmWwI5MBBuC6y-e7Ny9BBU',
+    authDomain: 'todolist-prod.firebaseapp.com',
+    databaseURL: 'https://todolist-prod.firebaseio.com',
+    projectId: 'todolist-prod',
+    storageBucket: 'todolist-prod.appspot.com',
+    messagingSenderId: '104714654432'
+  }
 };
